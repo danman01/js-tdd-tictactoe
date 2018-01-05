34028829a013404f462bdd22da5a9aaba7ecc9ef
Danny Kirschner <rkirschner1377@gmail.com>
2018-01-05 15:48:56 -0500

    Working on test driven front end javascript app
    Got some config options working for phantom JS to communicate with
    server
    Got reporter option working
    
    Saving token from server response
    
    Got chai-http implemented
    
    Next: test DOM content

--- spec/ticTacToe.spec.js
+++ spec/ticTacToe.spec.js
@@ -1,9 +1,14 @@
 //import q from 'q';
-const server = 'http://localhost:4741/'
+var server = 'http://localhost:4741/'
 //
 //chai.request.addPromises(q.Promise);
+var credentials = { credentials: {password: '123', password_confirmation: '123', email: 'test123@gmail.com' }}
+var token;
 describe('Home Page', function() {
-  it('should have a sign up button');
+  it('should have a sign up button', function(){
+    var signUpButton = document.getElementById('signUpButton')
+    (signUpButton.innerHTML).should.equal('Sign Up!')
+  })
   it('should have a sign in button');
   // when signed in
   it('should have a sign out button');
@@ -16,18 +21,16 @@ describe('Sign up', function() {
   it('should allow you to sign up with email and password', function(done) {
     chai.request(server)
       .post('sign-up')
-      .send({ credentials: {password: '123', password_confirmation: '123', email: 'test123@gmail.com' }})
+      .send( credentials )
       .end(function(err, res){
-        expect(res.status).to.equal(200)
-    //    let token = res.token
-    //    console.log(res)
+        console.log(res)
+        if(res.text.indexOf('has already been taken') >= 0){
+          //already signed up
+          expect(res.status).to.equal(400)
+        } else {
+          expect(res.status).to.equal(200)
+        }
         done()
-    //    //return agent
-    //    //  .get('games')
-    //    //  .set('Authentication authentication', token)
-    //    //  .then(function (res){
-    //    //    expect(res).to.have.status(200)
-    //    //  })
       })
   })
 })
@@ -35,21 +38,14 @@ describe('Sign in', function() {
   it('should allow you to sign in with email and password', function(done) {
     chai.request(server)
       .post('sign-in')
-      .send({ credentials: {password: '123', email: 'test123@gmail.com' }})
+      .send( credentials )
       .end(function(err, res){
-        console.log(res)
+        console.log("RESPONSE \n", res)
+        console.log("RESPONSE token \n", res.body.user.token)
+        token = res.body.user.token
         expect(res.status).to.equal(200)
         done()
-    //    let token = res.token
-    //    console.log(res)
-        //    done()
-    //    //return agent
-    //    //  .get('games')
-    //    //  .set('Authentication authentication', token)
-    //    //  .then(function (res){
-    //    //    expect(res).to.have.status(200)
-    //    //  })
-      })
+    })
   })
 })
 describe('TicTacToe Page', function() {
@@ -59,8 +55,10 @@ describe('TicTacToe Page', function() {
   it('should send a PATCH to the game api when a square is clicked to /games/:id');
   it('should send a PATCH to the game api when the game is over to /games/:id');
   it('should have a button to retreive all games with a GET to /games', function(done) {
+    console.log("TOKEN \n", token)
     chai.request(server)
       .get('games')
+      .set('Authorization', 'Token token=' + token)
       .end(function(err, res){
         expect(res.status).to.equal(200)  
         done()
---
+++ testing_notes.md
@@ -0,0 +1,2 @@
+to debug remotely in a webkit browser: "--remote-debugger-port": 9000
+then run __run() in a console, but how to step through and access debugger?
